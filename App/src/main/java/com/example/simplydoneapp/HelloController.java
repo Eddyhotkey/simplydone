896059fe2602f23e628907cacbb39e86fd11e564
Martin Frese <frese@men-at-work.de>
2024-02-13 21:24:14 +0100

    add action for buttons

--- App/src/main/java/com/example/simplydoneapp/HelloController.java
+++ App/src/main/java/com/example/simplydoneapp/HelloController.java
@@ -1,14 +1,42 @@
 package com.example.simplydoneapp;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.Objects;
 
 public class HelloController {
+    public Button formLageSubmit;
+    public Button formLageRegister;
+    public TextField formLageEmail;
+    public TextField formLagePassword;
     @FXML
     private Label welcomeText;
 
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    public void actLageSubmit(ActionEvent actionEvent) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("start-screen.fxml"));
+        Parent secondSceneRoot = loader.load();
+        Scene secondScene = new Scene(secondSceneRoot);
+
+        Scene currentScene = formLageSubmit.getScene();
+        Stage currentStage = (Stage) currentScene.getWindow();
+
+        if(Objects.equals(formLageEmail.getText(), "test")) {
+            if(Objects.equals(formLagePassword.getText(), "123")) {
+                currentStage.setScene(secondScene);
+            }
+        }
+    }
+
+    public void actLageRegister(ActionEvent actionEvent) {
+
     }
 }
