837fd4dda5acdace732aaffe29ec133aa3d83b67
Martin Frese <frese@men-at-work.de>
2024-02-25 01:28:16 +0100

    initial commit

---
+++ App/src/main/java/com/example/simplydoneapp/Task.java
@@ -0,0 +1,13 @@
+package com.example.simplydoneapp;
+
+import java.util.Date;
+
+public class Task {
+    String title;
+    String description;
+    Date dueDay;
+    String priority;
+    String category;
+
+
+}
