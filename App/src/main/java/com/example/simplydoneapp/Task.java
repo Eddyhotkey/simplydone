634814fe82b9ffb9ddf1ab1f65e5199f1fbe79d9
Martin Frese <frese@men-at-work.de>
2024-02-25 23:47:33 +0100

    add task class

--- App/src/main/java/com/example/simplydoneapp/Task.java
+++ App/src/main/java/com/example/simplydoneapp/Task.java
@@ -1,13 +1,66 @@
 package com.example.simplydoneapp;
 
+import java.time.LocalDate;
 import java.util.Date;
 
 public class Task {
+    int userID;
     String title;
     String description;
-    Date dueDay;
-    String priority;
+    LocalDate dueDay;
     String category;
+    String priority;
 
+    public Task(int userID, String title, String description, LocalDate dueDay, String category, String priority) {
+        this.userID = userID;
+        this.title = title;
+        this.description = description;
+        this.dueDay = dueDay;
+        this.category = category;
+        this.priority = priority;
+    }
 
+    public int getUserID() {
+        return userID;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public LocalDate getDueDay() {
+        return dueDay;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public String getPriority() {
+        return priority;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setDueDay(LocalDate dueDay) {
+        this.dueDay = dueDay;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public void setPriority(String priority) {
+        this.priority = priority;
+    }
 }
