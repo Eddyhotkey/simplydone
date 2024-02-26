37448753e4887f9cf71d68a97b17397f0e150c92
Martin Frese <frese@men-at-work.de>
2024-02-26 14:23:37 +0100

    add general function for api call and function for Todo call

--- App/src/main/java/com/example/simplydoneapp/Database.java
+++ App/src/main/java/com/example/simplydoneapp/Database.java
@@ -2,18 +2,62 @@ package com.example.simplydoneapp;
 
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
+import com.google.gson.reflect.TypeToken;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
 
+import java.lang.reflect.Type;
 import java.time.LocalDate;
-import java.util.Objects;
+import java.util.List;
 
 public  class Database {
     static String apiBaseUrl = "http:localhost:1337/";
     static OkHttpClient client = new OkHttpClient();
 
-    public static void setNewToDo(int userid, String title, String description, LocalDate dueday, String category, String priority) {
+    private static JsonObject apiCallSingleObject(String url) {
+        try {
+            Request request = new Request.Builder()
+                    .url(url)
+                    .build();
+            Response response = client.newCall(request).execute();
+
+            if (response.isSuccessful()) {
+                String responseBody = response.body().string();
+
+                Gson gson = new Gson();
+                JsonObject jsonObject = gson.fromJson(responseBody, JsonObject.class);
+                return jsonObject;
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        return null;
+    }
+
+    private static List<Task> apiCallMultipleObjects(String url) {
+        try {
+            Request request = new Request.Builder()
+                    .url(url)
+                    .build();
+            Response response = client.newCall(request).execute();
+
+            if (response.isSuccessful()) {
+                String responseBody = response.body().string();
+
+                // Definieren Sie den Typ für die Deserialisierung
+                Type listType = new TypeToken<List<Task>>() {}.getType();
+
+                Gson gson = new Gson();
+                return gson.fromJson(responseBody, listType);
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        return null;
+    }
+
+    public static int setNewToDo(int userid, String category, String title, String description, LocalDate dueday, String priority) {
         String apiUrl = apiBaseUrl + "todo/add_todo?"
                 + "userid="         + userid
                 + "&title="         + title
@@ -21,17 +65,18 @@ public  class Database {
                 + "&dueday="        + dueday
                 + "&category="      + category
                 + "&priority="      + priority;
-        try {
-            Request request = new Request.Builder()
-                    .url(apiUrl)
-                    .build();
-            Response response = client.newCall(request).execute();
 
-            if (response.isSuccessful()) {
-                String responseBody = response.body().string();
-            }
-        } catch (Exception e) {
-            System.out.println(e);
+        JsonObject apiResponse = apiCallSingleObject(apiUrl);
+
+        if(apiResponse != null) {
+            return apiResponse.get("ToDoID").getAsInt();
         }
+        return -1;
+    }
+    public static List<Task> getAllOpenToDos(int userid) {
+        String apiUrl = apiBaseUrl + "todo/get_all_open_todos?"
+                + "userid=" + userid;
+
+        return apiCallMultipleObjects(apiUrl);
     }
 }
