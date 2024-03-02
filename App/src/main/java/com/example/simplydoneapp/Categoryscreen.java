4e5a5ba1b6e335816e604351ec574df920e4781c
Martin Frese <frese@men-at-work.de>
2024-03-03 00:32:24 +0100

    add category screen functions

--- App/src/main/java/com/example/simplydoneapp/Categoryscreen.java
+++ App/src/main/java/com/example/simplydoneapp/Categoryscreen.java
@@ -2,32 +2,95 @@ package com.example.simplydoneapp;
 
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.control.DatePicker;
-import javafx.scene.control.Label;
+import javafx.scene.control.*;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
-public class Categoryscreen {
+import java.util.List;
 
-    protected void openCategory(Stage startscreen) {
-        AnchorPane test = new AnchorPane();
+public class Categoryscreen {
+    int userid;
+
+    protected void openCategory(Stage startscreen, int userid) {
+        this.userid = userid;
+
+        AnchorPane body = new AnchorPane();
         VBox container = new VBox();
+        VBox categoryList = new VBox();
+
+
         container.getStyleClass().add("category--container");
+
         container.setPrefWidth(700);
         container.setPrefHeight(700);
         container.setAlignment(Pos.CENTER);
+        body.setPrefWidth(Double.MAX_VALUE);
+        body.setPrefHeight(Double.MAX_VALUE);
 
-        Label labeldas = new Label();
-        labeldas.setText("Foo");
-        container.getChildren().add(labeldas);
+        HBox newCategoryBar = new HBox();
+        TextField newCategory = new TextField();
+        Button newCategorySubmit = new Button("add");
 
-        test.getChildren().add(container);
+        newCategory.setPromptText("Neue Kategorie");
+        newCategorySubmit.setOnAction(e -> actNewCategory(categoryList, newCategory.getText()));
 
-        Scene scene = new Scene(test, 1314, 799);
+        newCategoryBar.getChildren().addAll(newCategorySubmit, newCategory);
+        container.getChildren().add(newCategoryBar);
+
+
+        ScrollPane scrollCategory = new ScrollPane(categoryList);
+        loadCategories(categoryList);
+        container.getChildren().add(scrollCategory);
+
+
+
+        body.getChildren().add(container);
+
+        Scene scene = new Scene(body, 1314, 799);
         startscreen.setScene(scene);
         scene.getStylesheets().add(getClass().getResource("styles/css/base.css").toExternalForm());
         startscreen.show();
+    }
+
+    protected void actNewCategory(VBox categoryVBox, String name) {
+        Category newCategory = new Category(Database.addNewCategory(this.userid, name), name);
+        addCategory(categoryVBox, newCategory);
+    }
+
+    protected void loadCategories(VBox categoryVBox) {
+        List<Category> categoryList = Database.getAllCategories(this.userid);
+
+        if (categoryList != null) {
+            for (Category category : categoryList) {
+                addCategory(categoryVBox, category);
+            }
+        }
+    }
+
+    protected void shareCategory() {
+        //ToDO
+    }
+
+    protected void deleteCategory() {
+        //ToDO
+    }
+
+    private void addCategory(VBox categoryVBox, Category category) {
+        HBox container = new HBox();
+
+        Label categoryID = new Label(String.valueOf(category.getCategoryID()));
+        Label categoryName = new Label(category.getKategoriename());
+
+        Button shareCategory = new Button("share");
+        shareCategory.setOnAction(e -> shareCategory());
+
+        Button deleteCategory = new Button("del");
+        deleteCategory.setOnAction(e -> deleteCategory());
+
+        container.getChildren().addAll(categoryID, categoryName, shareCategory, deleteCategory);
+        categoryVBox.getChildren().add(container);
     }
 
 
