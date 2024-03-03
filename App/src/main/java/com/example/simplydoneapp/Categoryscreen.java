21e2ab4f027346e4acaaaea387703a994126acdb
Martin Frese <frese@men-at-work.de>
2024-03-03 13:01:46 +0100

    add topbar buttons and fix position center

--- App/src/main/java/com/example/simplydoneapp/Categoryscreen.java
+++ App/src/main/java/com/example/simplydoneapp/Categoryscreen.java
@@ -3,11 +3,18 @@ package com.example.simplydoneapp;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.Objects;
 
@@ -18,17 +25,56 @@ public class Categoryscreen {
         this.userid = userid;
 
         AnchorPane body = new AnchorPane();
+        body.getStyleClass().add("body");
         VBox container = new VBox();
         VBox categoryList = new VBox();
 
-
-        container.getStyleClass().add("category--container");
-
-        container.setPrefWidth(700);
-        container.setPrefHeight(700);
-        container.setAlignment(Pos.CENTER);
         body.setPrefWidth(Double.MAX_VALUE);
         body.setPrefHeight(Double.MAX_VALUE);
+
+        container.setAlignment(Pos.CENTER);
+        container.setPrefWidth(700);
+        container.setPrefHeight(500);
+        categoryList.setPrefWidth(650);
+        categoryList.setPrefHeight(450);
+
+        categoryList.setMaxSize(700, 700);
+        container.setMaxSize(700, 700);
+
+        AnchorPane.setTopAnchor(container, (body.getHeight() - container.getHeight()) / 2);
+        AnchorPane.setBottomAnchor(container, (body.getHeight() - container.getHeight()) / 2);
+        AnchorPane.setLeftAnchor(container, (body.getWidth() - container.getWidth()) / 2);
+        AnchorPane.setRightAnchor(container, (body.getWidth() - container.getWidth()) / 2);
+
+        Button logoButton = new Button();
+        logoButton.getStyleClass().add("button--image");
+        Image imageLogo = new Image(getClass().getResource("images/Logo_simplyDone.png").toExternalForm());
+        ImageView imageView = new ImageView(imageLogo);
+        logoButton.setGraphic(imageView);
+
+        body.getChildren().add(logoButton);
+        AnchorPane.setTopAnchor(logoButton, 0.0);
+        AnchorPane.setLeftAnchor(logoButton, 0.0);
+
+        ButtonBar topButtonBar = new ButtonBar();
+
+        Button newTask = new Button();
+        newTask.getStyleClass().addAll("button--image", "button--image--round");
+        Image imageNewTask = new Image(getClass().getResource("images/plus-circle.png").toExternalForm());
+        ImageView imageViewNewTask = new ImageView(imageNewTask);
+        newTask.setGraphic(imageViewNewTask);
+
+        Button exit = new Button();
+        exit.getStyleClass().add("button--image");
+        Image imageExit = new Image(getClass().getResource("images/log-out.png").toExternalForm());
+        ImageView imageViewExit = new ImageView(imageExit);
+        exit.setGraphic(imageViewExit);
+
+        topButtonBar.getButtons().addAll(newTask, exit);
+
+        body.getChildren().add(topButtonBar);
+        AnchorPane.setTopAnchor(topButtonBar, 0.0);
+        AnchorPane.setRightAnchor(topButtonBar, 0.0);
 
         HBox newCategoryBar = new HBox();
         TextField newCategory = new TextField();
@@ -42,10 +88,13 @@ public class Categoryscreen {
 
 
         ScrollPane scrollCategory = new ScrollPane(categoryList);
+
+        scrollCategory.setPrefWidth(700);
+        scrollCategory.setPrefHeight(500);
+        scrollCategory.setMaxSize(700, 500);
+
         loadCategories(categoryList);
         container.getChildren().add(scrollCategory);
-
-
 
         body.getChildren().add(container);
 
